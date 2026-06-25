a6ab6f55a89306454e1de67e7c35c4090f64ae69
weebdev123 <166636609+weebdev123@users.noreply.github.com>
2026-06-25 16:48:08 +0530

    Added list of letters, lives, and a while loop for the game. In while loop, player makes a guess which is searched in list of letters. covered cases such as letter found or not found in list, or when lives over or letters over.

--- main.py
+++ main.py
@@ -5,4 +5,21 @@ print("Welcome to Hangman!") #greeting
 word_list = ["apple", "banana", "melon", "orange", "mango"] #list of word to choose from
 
 word = random.choice(word_list) #chooses a random word
-print(word) #prints to showcase for now
+
+letters = list(word) #stores list of letters
+lives = 5 #total lives
+while True: #while loop runs until break
+    guess = input("Guess a letter: ") #stores guess
+    if guess in letters: #checks if guess is found in letters
+        letters.remove(guess) #removes the letter's first occurence
+        print("Your letter was found!")
+    else:
+        lives -= 1 #one life gone when incorrect guess
+        print("Your letter was not found! One life gone")
+
+    if lives == 0: #break condition when lives over
+        print("Your Lives are gone. You Lose!")
+        break
+    elif not letters: #break condition when no letters remain
+        print("You guessed all letters. You Win!")
+        break
